9ce95a3b8c20d9e3b9c8f87c7ba52d6a8485a501
xiwei <xwei@utexas.edu>
2025-03-27 17:40:59 -0500

    Move GreetingScreen to feature:greeting

--- feature/greeting/build.gradle.kts
+++ feature/greeting/build.gradle.kts
@@ -42,6 +42,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
 
-//    debugImplementation(libs.androidx.ui.tooling)
-//    debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
